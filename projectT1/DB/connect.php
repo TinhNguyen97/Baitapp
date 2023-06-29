31393a12099f2a41c7ea4d747bc3956e304a6dcd
TinhNguyen97 <princetinkie97@gmail.com>
2023-06-29 16:39:14 +0700

    them env

--- projectT1/DB/connect.php
+++ projectT1/DB/connect.php
@@ -1,11 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-// header('Content-Type: application/json');
+require_once '../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createMutable(substr(__DIR__, 0, -2));
+$dotenv->load();
+echo $_ENV['DBNAME'];
+$servername = $_ENV['SERVERNAME'];
+$username = $_ENV['USERNAME'];
+$password = $_ENV['PASSWORD'];
+$dbname = $_ENV['DBNAME'];
+$port = $_ENV['PORT'];
 header('Access-Control-Allow-Origin: *');
 try {
-  $conn = new PDO("mysql:host=127.0.0.1;dbname=tinhtest;port=3307", $username);
+  $conn = new PDO("mysql:host=127.0.0.1;dbname=$dbname;port=$port", $username);
 } catch (Exception $e) {
   echo [$e->getMessage()];
 }
